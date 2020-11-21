ea916a643cc3c90825c4a71e98463ded9199f83b
phuongdiep <phuongdiep@kms-technology.com>
2020-11-21 15:35:08 +0700

    - Add actions selectAllResources & unselectAllResources on RL page

--- cypress/integration/resource.library.spec.js
+++ cypress/integration/resource.library.spec.js
@@ -6,31 +6,52 @@ import ResourceLibraryPage from '../page-objects/ResourceLibraryPage'
 const loginData = require('../data/Backdoor.json');
 const backDoor = require('../data/Backdoor.json');
 
-describe("Go to Resource Library Page successfully", () => {
-  const coursePlan = new CoursePlanPage();
-  beforeEach(()=>{
-    const loginPage = new LoginPage();
-    loginPage.launchCourse(backDoor.email, backDoor.course1)
-  })
+// describe("Go to Resource Library Page successfully", () => {
+//   const coursePlan = new CoursePlanPage();
+//   beforeEach(()=>{
+//     const loginPage = new LoginPage();
+//     loginPage.launchCourse(backDoor.email, backDoor.course1)
+//   })
     
-    it("Launch Resource Library page by clicking Resource Ebook from navigation bar", () => {
-      coursePlan.openResourcesPageByNavigationBar()
-      coursePlan.verifyResourcePageIsOpenSuccess()
-    });
+//     it("Launch Resource Library page by clicking Resource Ebook from navigation bar", () => {
+//       coursePlan.openResourcesPageByNavigationBar()
+//       coursePlan.verifyResourcePageIsOpenSuccess()
+//     });
 
-    it("Launch RL page by clicking Addmoreresources from Top menu", () => {
-      coursePlan.openResourcesPageByTopMenu()
-      coursePlan.verifyResourcePageIsOpenSuccess()
-    });
+//     it("Launch RL page by clicking Addmoreresources from Top menu", () => {
+//       coursePlan.openResourcesPageByTopMenu()
+//       coursePlan.verifyResourcePageIsOpenSuccess()
+//     });
 
-    it('Launch RL page by clicking AddAResource from an empty folder', () => {
-      coursePlan.openResourcePageFromEmptyFolder()
-      coursePlan.verifyResourcePageIsOpenSuccess()
-    });
+//     it('Launch RL page by clicking AddAResource from an empty folder', () => {
+//       coursePlan.openResourcePageFromEmptyFolder()
+//       coursePlan.verifyResourcePageIsOpenSuccess()
+//     });
 
-    it('Launch RL page by clicking AddAResource from action menu', () => {
+//     it('Launch RL page by clicking AddAResource from action menu', () => {
       
+//     });
+
+//   });
+
+  describe("", () => {
+    const resourceLibrary = new ResourceLibraryPage()
+
+    beforeEach(()=>{
+      const coursePlan = new CoursePlanPage();
+      const loginPage = new LoginPage();
+      loginPage.launchCourse(backDoor.email, backDoor.course1)
+      coursePlan.openResourcesPageByNavigationBar()
+    })
+
+    it('Verify Resource Library page header', () => {
+      resourceLibrary.verifyPageHeader()
+      resourceLibrary.selectAllResources()
+      resourceLibrary.unselectAllResources()
     });
 
 
-  });
+
+
+
+  })
--- cypress/page-objects/ResourceLibraryPage.js
+++ cypress/page-objects/ResourceLibraryPage.js
@@ -1,7 +1,25 @@
 
 const ResourceLibraryData = require('../data/ResourceLibrary.json');
 
+//
+const header = '.o-els-flex-layout__item >h4'
+
+//Checkboxes
+const chkSelectAll = "[name='checkbox_select-all']"
+
 class ResourceLibraryPage {
+    verifyPageHeader(){
+        cy.get(header).should('have.text', 'Resource Library')
+    }
+
+    selectAllResources(){
+        cy.get(chkSelectAll).check({force:true}).should('be.checked')
+    }
+
+    unselectAllResources(){
+        cy.get(chkSelectAll).uncheck({force:true}).should('not.be.checked')
+    }
+    
         
 
 
