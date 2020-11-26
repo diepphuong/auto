e72a9203811dd31ece9d9b22da128d37504b71ed
Lam Doi <32857609+LamDoi@users.noreply.github.com>
2020-11-26 09:29:57 +0700

    add sandbox

---
+++ cypress/integration/Sandbox/lam.sandbox.spec.js
@@ -0,0 +1,46 @@
+import LoginPage from '../page-objects/LoginPage'
+import CoursePlanPage from '../page-objects/CoursePlanPage'
+
+//Data
+const backDoor = require('../data/Backdoor.json')
+const coursePlan = require('../data/CoursePlan.json')
+
+describe ('Verify Course Plan page', ()=>{
+  const coursePlanPage = new CoursePlanPage()
+
+  beforeEach(()=>{
+    const loginPage = new LoginPage()
+    loginPage.launchCourse(backDoor.email_lam, backDoor.course_lam)
+  })
+
+  it("Verify UI course plan page", () => {
+    coursePlanPage.verifyUICoursePlan()
+  })
+
+})
+
+describe ('Verify New Folder modal', ()=>{
+  const coursePlanPage = new CoursePlanPage()
+
+  beforeEach(()=>{
+    const loginPage = new LoginPage()
+    loginPage.launchCourse(backDoor.email_lam, backDoor.course_lam)
+  })
+
+  it("Verify UI course plan page", () => {
+    coursePlanPage.verifyUICoursePlan()
+  })
+
+  it("Verify Add New Folder from top menu", () => {
+    coursePlanPage.addParentFolder(coursePlan.folderName)
+  })
+
+  it("Verify Add Sub Folder from top menu", () => {
+    coursePlanPage.addSubFolder(coursePlan.subFolder, '- ' + coursePlan.folderName, coursePlan.insertLocation)
+  })
+
+  it("Verify Delete folder from Coure Plan", () => {
+    coursePlanPage.removeItemsFromCoursePlan(coursePlan.folderName)
+  })
+
+})
